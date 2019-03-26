acad80124cabafb589f47fec6cd7768793da8a8e
michielstock <michielfmstock@gmail.com>
2019-03-26 15:29:51 +0100

    :art: finalize for lecture

--- Chapters/07.ShortestPathProblems/parcour_tools.py
+++ Chapters/07.ShortestPathProblems/parcour_tools.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed 6 Apr 2016
-Last update on Thu 7 Apr 2016
+Last update on Tue 23 Mar 2019
 
 @author: michielstock
 
@@ -11,7 +11,7 @@ for shortest path algorithms
 
 import numpy as np
 from random import choice, randint
-from shortestpaths import red
+from shortestpaths import red, blue, yellow
 
 def links_to_graph(links):
     """
@@ -34,14 +34,14 @@ def plot_parcour(links, ax, line_width=5):
     max_x = 0
     max_y = 0
     for (i1, j1), (i2, j2) in links:
-        ax.plot([i1, i2], [j1, j2], c='white', lw=line_width)
+        ax.plot([i1, i2], [j1, j2], c=yellow, lw=line_width)
         if max(i1, i2) > max_x:
             max_x = max(i1, i2) + 0.5
         if max(j1, j2) > max_y:
             max_y = max(j1, j2) + 0.5
     ax.set_xlim([-0.5, max_x])
     ax.set_ylim([-0.5, max_y])
-    ax.set_axis_bgcolor('black')
+    #ax.set_axis_bgcolor('black')
 
 def add_path(path, ax, color=red):
     """
